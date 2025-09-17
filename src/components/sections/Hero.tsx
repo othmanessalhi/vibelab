36049dec3f30914cc059907a73c435dd55cb89ba
Othman Essalhi <otman9005@gmail.com>
2025-09-17 21:58:32 +0000

    place another dark balneciaga image

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
         }
       >
         <Image
-          src="https://images.unsplash.com/photo-1581338834647-b0fb40702de3?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="hero"
           height={720}
           width={1400}
