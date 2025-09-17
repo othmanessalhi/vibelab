534ca47bd73088e5ebef9282e229e302a8b5ae51
Othman Essalhi <otman9005@gmail.com>
2025-09-17 20:54:47 +0000

    center it

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,7 +3,7 @@ import { RevealImageList } from "@/components/ui/reveal-images"
 export default function Hero() {
   return (
     <section className="min-h-screen flex items-center justify-center bg-background">
-      <div className="container mx-auto px-4 md:px-6">
+      <div className="container mx-auto px-4 md:px-6 flex justify-center">
         <RevealImageList />
       </div>
     </section>
