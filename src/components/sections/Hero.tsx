8d8b2e1689774639559a64e4689f98348897a397
Othman Essalhi <otman9005@gmail.com>
2025-09-17 21:55:50 +0000

    change the image to somthing about fashion (_for element img_)

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
         }
       >
         <Image
-          src="https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="hero"
           height={720}
           width={1400}
