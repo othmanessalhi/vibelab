834178e57f5052d2c7c0bd25ff8eb2a0fe34e1cd
Othman Essalhi <otman9005@gmail.com>
2025-09-18 23:37:28 +0000

    remove the our services seperated page

--- src/components/ui/interactive-scrolling-story-component.tsx
+++ src/components/ui/interactive-scrolling-story-component.tsx
@@ -54,7 +54,7 @@ const SlideContent: React.FC<SlideContentProps> = ({ index, slide, activeIndex }
       <div className="overflow-hidden mt-8">
         <motion.div variants={contentChildVariants}>
             <Button asChild size="lg">
-                <Link href="/services">Learn More</Link>
+                <Link href="/#services">Learn More</Link>
             </Button>
         </motion.div>
       </div>
