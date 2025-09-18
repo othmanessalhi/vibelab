b16607d64b83df8f3f69db33e5e929a8764830c6
Othman Essalhi <otman9005@gmail.com>
2025-09-18 15:50:11 +0000

    not working

--- src/components/ui/reveal-images.tsx
+++ src/components/ui/reveal-images.tsx
@@ -12,31 +12,29 @@ interface ShowImageListItemProps {
 }
 
 function RevealImageListItem({ text, images }: ShowImageListItemProps) {
-  const imageContainerBase = "absolute top-1/2 -translate-y-1/2 z-10 h-40 w-40 overflow-hidden rounded-md shadow-xl transition-all duration-500 scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100";
+  const imageContainerBase = "absolute top-1/2 -translate-y-1/2 z-10 h-40 w-40 overflow-hidden rounded-md shadow-xl transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 scale-90 group-hover:scale-100";
 
   return (
-    <div className="group relative w-full cursor-pointer border-b border-border/20">
-      <div className="py-8">
-        <h1 className="text-5xl font-black text-left text-foreground transition-all duration-500 group-hover:text-accent">
-            {text}
-        </h1>
-      </div>
+    <div className="group relative w-full cursor-pointer border-b border-border/20 py-8">
+      <h1 className="text-5xl font-black text-left text-foreground transition-all duration-300 group-hover:text-accent">
+          {text}
+      </h1>
       <div
         className={cn(
           imageContainerBase,
-          "right-0 translate-x-[10%]",
-        )}
-      >
-        <img alt={images[1].alt} src={images[1].src} className="h-full w-full object-cover" />
-      </div>
-      <div
-        className={cn(
-          imageContainerBase,
-          "right-0 translate-x-[40%]",
-          "transition-all delay-150 duration-500 group-hover:rotate-6"
+          "right-48",
+          "group-hover:rotate-6"
         )}
       >
         <img alt={images[0].alt} src={images[0].src} className="h-full w-full object-cover" />
+      </div>
+      <div
+        className={cn(
+          imageContainerBase,
+          "right-24"
+        )}
+      >
+        <img alt={images[1].alt} src={images[1].src} className="h-full w-full object-cover" />
       </div>
     </div>
   );
