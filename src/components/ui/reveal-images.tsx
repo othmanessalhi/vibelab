c4869edf0cb4dd8e2dfaca8d2e6bc4f344ccb0a7
Othman Essalhi <otman9005@gmail.com>
2025-09-18 15:38:52 +0000

    when i hover the mouse i don't see the images

--- src/components/ui/reveal-images.tsx
+++ src/components/ui/reveal-images.tsx
@@ -13,7 +13,7 @@ interface ShowImageListItemProps {
 function RevealImageListItem({ text, images }: ShowImageListItemProps) {
   const container = "absolute right-8 -top-1 z-40 h-20 w-16";
   const effect =
-    "relative duration-500 delay-100 shadow-none group-hover:shadow-xl scale-0 group-hover:scale-100 opacity-0 group-hover:opacity-100 group-hover:w-full group-hover:h-full w-16 h-16 overflow-hidden transition-all rounded-md";
+    "relative transition-all duration-500 delay-100 shadow-none w-16 h-16 overflow-hidden rounded-md group-hover:shadow-xl group-hover:w-full group-hover:h-full";
 
   return (
     <div className="group relative h-fit w-full cursor-pointer overflow-visible py-8">
@@ -21,7 +21,7 @@ function RevealImageListItem({ text, images }: ShowImageListItemProps) {
         {text}
       </h1>
       <div className={container}>
-        <div className={effect}>
+        <div className={cn(effect, "scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100")}>
           <img alt={images[1].alt} src={images[1].src} className="h-full w-full object-cover" />
         </div>
       </div>
@@ -31,7 +31,7 @@ function RevealImageListItem({ text, images }: ShowImageListItemProps) {
           "translate-x-0 translate-y-0 rotate-0 transition-all delay-150 duration-500 group-hover:translate-x-6 group-hover:translate-y-6 group-hover:rotate-12",
         )}
       >
-        <div className={cn(effect, "duration-200")}>
+        <div className={cn(effect, "duration-200", "scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100")}>
           <img alt={images[0].alt} src={images[0].src} className="h-full w-full object-cover" />
         </div>
       </div>
