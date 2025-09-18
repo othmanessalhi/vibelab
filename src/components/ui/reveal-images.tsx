3f1893e2162f069c279b1d3c352bc60997f123e6
Othman Essalhi <otman9005@gmail.com>
2025-09-18 15:51:49 +0000

    not working

--- src/components/ui/reveal-images.tsx
+++ src/components/ui/reveal-images.tsx
@@ -12,29 +12,29 @@ interface ShowImageListItemProps {
 }
 
 function RevealImageListItem({ text, images }: ShowImageListItemProps) {
-  const imageContainerBase = "absolute top-1/2 -translate-y-1/2 z-10 h-40 w-40 overflow-hidden rounded-md shadow-xl transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 scale-90 group-hover:scale-100";
+  const container = "absolute right-8 -top-1 z-40 h-20 w-16";
+  const effect =
+    "relative duration-500 delay-100 shadow-none group-hover:shadow-xl scale-0 group-hover:scale-100 opacity-0 group-hover:opacity-100 group-hover:w-full group-hover:h-full w-16 h-16 overflow-hidden transition-all rounded-md";
 
   return (
-    <div className="group relative w-full cursor-pointer border-b border-border/20 py-8">
-      <h1 className="text-5xl font-black text-left text-foreground transition-all duration-300 group-hover:text-accent">
-          {text}
+    <div className="group relative h-fit w-fit overflow-visible py-8">
+      <h1 className="text-7xl font-black text-foreground transition-all duration-500 group-hover:opacity-40">
+        {text}
       </h1>
-      <div
-        className={cn(
-          imageContainerBase,
-          "right-48",
-          "group-hover:rotate-6"
-        )}
-      >
-        <img alt={images[0].alt} src={images[0].src} className="h-full w-full object-cover" />
+      <div className={container}>
+        <div className={effect}>
+          <img alt={images[1].alt} src={images[1].src} className="h-full w-full object-cover" />
+        </div>
       </div>
       <div
         className={cn(
-          imageContainerBase,
-          "right-24"
+          container,
+          "translate-x-0 translate-y-0 rotate-0 transition-all delay-150 duration-500 group-hover:translate-x-6 group-hover:translate-y-6 group-hover:rotate-12",
         )}
       >
-        <img alt={images[1].alt} src={images[1].src} className="h-full w-full object-cover" />
+        <div className={cn(effect, "duration-200")}>
+          <img alt={images[0].alt} src={images[0].src} className="h-full w-full object-cover" />
+        </div>
       </div>
     </div>
   );
@@ -42,7 +42,7 @@ function RevealImageListItem({ text, images }: ShowImageListItemProps) {
 
 function RevealImageList({items}: {items: ShowImageListItemProps[]}) {
   return (
-    <div className="flex w-full max-w-2xl flex-col gap-1 rounded-sm bg-background px-8 py-4">
+    <div className="flex flex-col gap-1 rounded-sm bg-background px-8 py-4">
       <h3 className="text-sm font-black uppercase text-muted-foreground">Our services</h3>
       {items.map((item, index) => (
         <RevealImageListItem key={index} text={item.text} images={item.images} />
