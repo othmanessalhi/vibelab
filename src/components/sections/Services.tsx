4ffe56bfb24cb4c94d62ccf9238c514109d66b11
Othman Essalhi <otman9005@gmail.com>
2025-09-18 01:42:47 +0000

    it's not responsive also it does'nt display our infos i can't scroll or

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -33,7 +33,7 @@ const SLIDES = [
     services: ['PPC', 'Social Ads', 'Analytics'],
     type: 'Advertising',
     imageUrl:
-      'https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=2612&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=2612&auto=format&fit=crop&ixlib-rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'slide-3',
