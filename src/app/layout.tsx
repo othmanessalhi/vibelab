1901a2a8892c204c76ec9a032824b44b4745157f
Othman Essalhi <otman9005@gmail.com>
2025-10-05 22:38:51 +0000

    still having trouble

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,7 +124,7 @@ export default function RootLayout({
           <Footer />
           <Toaster />
         </ThemeProvider>
-        <Suspense>
+        <Suspense fallback={null}>
           <Analytics />
         </Suspense>
       </body>
