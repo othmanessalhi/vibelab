3af8361be6d8d70b48da163718ae1bd704fb022f
Othman Essalhi <otman9005@gmail.com>
2025-10-05 03:10:13 +0000

    add this section after about us You are given a task to integrate an exi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import WhoAreWe from "@/components/sections/WhoAreWe";
 import Services from "@/components/sections/Services";
 import Testimonials from "@/components/sections/Testimonials";
 import MovingText from "@/components/sections/MovingText";
+import { ShuffleHero } from "@/components/ui/shuffle-grid";
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
       <Hero />
       <AnimateIn>
         <WhoAreWe />
+      </AnimateIn>
+      <AnimateIn>
+        <ShuffleHero />
       </AnimateIn>
       <Services />
       <AnimateIn>
